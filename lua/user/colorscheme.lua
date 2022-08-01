9880801ab1310b7e2cf7aa5349b6b6702bbcc3fa
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-08-01 19:58:43 +0900

    :lipstick: change colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,22 @@
 local colorscheme = "duskfox"
 
+local nf_status_ok, nightfox = pcall(require, "nightfox")
+if not nf_status_ok then
+  return
+end
+
+nightfox.setup({
+  options = {
+    dim_inactive = true,
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      conditionals = "bold",
+      functions = "bold"
+    }
+  }
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
