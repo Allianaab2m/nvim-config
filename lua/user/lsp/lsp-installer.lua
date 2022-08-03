b26702c68ef4f4889ac864a137a5ec7429f92c9b
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-08-03 21:05:23 +0900

    :wrench: add lsp update

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -81,3 +81,9 @@ for _, server in pairs(servers) do
 
   lspconfig[server].setup(opts)
 end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    update_in_insert = true
+  }
+)
